b42576faeb7379275bbc1a13c87b662dc0b6ae47
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2022-09-16 12:50:32 +0200

    Reapply -b to legacy release version.

--- wooey/management/commands/addscript.py
+++ wooey/management/commands/addscript.py
@@ -54,9 +54,10 @@ class Command(BaseCommand):
                 sys.stdout.write('Converting {}\n'.format(script))
                 # copy the script to our storage
                 base_name = options.get('name') or os.path.splitext(os.path.split(script)[1])[0]
-                with open(script, 'r') as f:
-                    script = default_storage.save(os.path.join(wooey_settings.WOOEY_SCRIPT_DIR, os.path.split(script)[1]), File(f))
-                    if wooey_settings.WOOEY_EPHEMERAL_FILES:
+                with open(script, 'rb') as f:
+                    script_path = default_storage.save(os.path.join(wooey_settings.WOOEY_SCRIPT_DIR, os.path.split(script)[1]), File(f))
+                if wooey_settings.WOOEY_EPHEMERAL_FILES:
+                    with open(script, 'rb') as f:
                         # save it locally as well (the default_storage will default to the remote store)
                         local_storage = get_storage(local=True)
                         local_storage.save(os.path.join(wooey_settings.WOOEY_SCRIPT_DIR, os.path.split(script)[1]), File(f))
